c4cac0cc7c924b475d203a90eb362222b5e76efe
Roxanne Gutierrez <roxannenguyenn@gmail.com>
2023-01-10 15:44:25 -0600

    commit tree f4d232dc0340b4f5c3c62139976938e886e97011

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+/*
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+*/
+
+const superbowlWin = (recordObj) => {
+  const winner = recordObj.find((win) => win.result.toUpperCase() === "W");
+  if (winner) {
+    return winner.year;
+  } else {
+    return undefined;
+  }
+};
